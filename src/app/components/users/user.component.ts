34b4e1c0a552b0c79702687de205ab37d02d2666
Gregory Lebret <greg@lglab.co.uk>
2016-10-26 09:30:43 +0100

    get user id from route params

--- src/app/components/users/user.component.ts
+++ src/app/components/users/user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpService } from '../../services/http.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -14,20 +15,33 @@ import { HttpService } from '../../services/http.service';
 export class UserComponent {
 
   public user: any;
+  private sub: any;
 
-  constructor(private httpService: HttpService) {
+  id: number;
+
+  constructor(
+    private httpService: HttpService,
+    private route: ActivatedRoute
+  ) {
 
   }
 
   ngOnInit() {
-    this.getUser();
+    this.sub = this.route.params.subscribe(params => {
+       this.id = +params['id'];
+       this.getUser();
+    });
   }
  
   getUser() {
-    this.httpService.get('https://jsonplaceholder.typicode.com/users/1').subscribe(
+    this.httpService.get('https://jsonplaceholder.typicode.com/users/' + this.id).subscribe(
       data => { this.user = data },
       err => { console.error(err) }
     );
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 };
